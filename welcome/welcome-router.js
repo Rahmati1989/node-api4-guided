1e05b4cba1a2a3335c97ec9202d7a540bf21713c
Rafiullah Rahmati <rafiullahrahmati@Rafiullahs-MacBook-Pro.local>
2021-02-18 14:28:54 -0800

    back to fact

--- welcome/welcome-router.js
+++ welcome/welcome-router.js
@@ -5,7 +5,7 @@ const router = express.Router()
 router.get("/", (req, res) => {
 	res.status(200).json({
 		message: (`Welcome ${process.env.COHORT}`),
-		message: process.env.COHORT || "I have no fun facts"
+		fact: process.env.COHORT || "I have no fun facts"
 	})
 })
 
